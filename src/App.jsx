4e441d54b18cacf85939d3cf83eeea3991890a1b
pavankumarpilaka <143637501+pavankumarpilaka@users.noreply.github.com>
2025-03-10 14:33:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Login from './pages/Login'
 import Contact from './pages/Contact'
 import Myprofile from './pages/Myprofile'
