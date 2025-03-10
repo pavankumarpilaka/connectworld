15c79ab07d879f1fd899c35f2ac066ad28b43130
pavan1 <pavankumarpilaka96@gmail.com>
2025-03-10 14:32:20 +0530

    complete frontend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import profile_pic from '../assets/profile_pic.png'
 import dd_icon from '../assets/dropdown_icon.svg'
 import { useState } from 'react'
 import { AppContext } from '../context/AppContext'
+import { assets } from '../assets/assets'
 
 
 const Navbar = () => {
@@ -40,10 +41,26 @@ const Navbar = () => {
                     <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
                         <p onClick={()=>navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
                         <p onClick={()=>navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
-                        <p onClick={()=>settoken(false)} className='hover:text-black cursor-pointer'>Logout</p>
+                        <p onClick={()=>{settoken(false),navigate('/')}} className='hover:text-black cursor-pointer' >Logout</p>
                     </div>
                 </div>
             </div>:<button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>}
+            <img onClick={()=>setshowmenu(true)} className='w-6 md:hidden ' src={assets.menu_icon} alt=''/>
+            <div className={`${showmenu?'fixed w-full':'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+                <div className='flex items-center justify-between px-5 py-6'>
+                    <img className=''w-36 src={assets.logo} alt=''/>
+                    <img className='w-7' onClick={()=>setshowmenu(false)} src={assets.cross_icon} alt=''/>
+                </div>
+                <ul className='flex flex-col items-center font-medium gap-2 px-5 text-lg'>
+                    <NavLink className='px-4 py-2 rounded inline-block' to='/' onClick={()=>setshowmenu(false)}>Home</NavLink>
+                    <NavLink className='px-4 py-2 rounded inline-block' to='/Influencers' onClick={()=>setshowmenu(false)}>Influencers</NavLink>
+                    <a className='px-4 py-2 rounded inline-block' href="#foot" onClick={()=>setshowmenu(false)}>About</a>
+                    <a className='px-4 py-2 rounded inline-block' href="#foot" onClick={()=>setshowmenu(false)}>Contact Us</a>
+                    {
+                        token?<p></p>:<button onClick={()=>{navigate('/login'),setshowmenu(false)}} className='text-green-500'>Create Account</button>
+                    }
+                </ul>
+            </div>
         </div>
     </div>
   )
--- src/pages/Myappointments.jsx
+++ src/pages/Myappointments.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Myappointments = () => {
+  const {Influencers}=useContext(AppContext)
   return (
-    <div>Myappointments</div>
+    <div>
+      <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My Appointements</p>
+      <div>
+       {
+        Influencers.slice(0,2).map((ite,index)=>(
+          <div className='grid-grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
+             <div>
+               <img className='w-32 bg-indigo-50' src={ite.image} alt=''/>
+             </div>
+             <div className='flex-1 text-sm text-zinc-600 items-center'>
+              <p className='text-neural font-semibold'>{ite.name}</p>
+              <p>{ite.degree}</p>
+              <p>{ite.speciality}</p>
+              <p className='text-sm mt-2'><span className='text-sm text-neutral-700 font-medium' >Date & Time:</span> 25, March,2025 | 8:00 PM</p>
+             </div>
+             <div></div>
+             <div className='flex flex-col gap-2 align-items-center'><button className='text-sm text-stone-500 text-center sm-min-w-48 py-2 px-4 border rounded-full hover:bg-red-600 hover:text-black' >Cancel Appointment</button></div>
+
+          </div>
+        ))
+       }
+      </div>
+    </div>
   )
 }
 
